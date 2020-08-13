895c1064ccfa1cf66f7ba81f58f34c4b48f5796f
Nikita Oksak <stupidmaadworld@gmail.com>
2020-08-14 02:47:44 +0300

    Updates

---
+++ precache-manifest.219c7d9f54111f29cc2ef2a28a8b77f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe0c03fd8ab54b96b1b327ca24587fc",
+    "url": "/solution_swap/index.html"
+  },
+  {
+    "revision": "f306ba302b0446cf3932",
+    "url": "/solution_swap/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1cc35019d02ef2c4d47f",
+    "url": "/solution_swap/static/css/main.5fade1d8.chunk.css"
+  },
+  {
+    "revision": "f306ba302b0446cf3932",
+    "url": "/solution_swap/static/js/2.77f09a71.chunk.js"
+  },
+  {
+    "revision": "ccfb27d768df50df1edb1a2bb04b68be",
+    "url": "/solution_swap/static/js/2.77f09a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc35019d02ef2c4d47f",
+    "url": "/solution_swap/static/js/main.f2dd8057.chunk.js"
+  },
+  {
+    "revision": "c464508f2e451b2bb809",
+    "url": "/solution_swap/static/js/runtime-main.93fd88a4.js"
+  }
+]);
