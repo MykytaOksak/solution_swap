61aeaf7fea0a5f57bf56a4c88c07729c84834476
Nikita Oksak <stupidmaadworld@gmail.com>
2020-08-14 02:34:59 +0300

    edit

--- src/components/PB/PB.jsx
+++ src/components/PB/PB.jsx
@@ -1,41 +1,9 @@
 import React from 'react'
 import CreatePost from './../CreatePost/CreatePost'
 import PostCard from '../PostCard/PostCard'
-/*
 import TASKS from './../shared/tasks'
-*/
-
-const TASKS = [
-    {
-        id: 1,
-        type: 'PB',
-        title: 'activeClassName attribute refuses to work',
-        description: 'The attribute has no impact on how the NavLink looks'
-    },
-    {
-        id: 2,
-        type: 'MathStat',
-        title: 'Calculation of ...',
-        description: 'Help me to calculate the probability of ...'
-    },
-    {
-        id: 3,
-        type: 'MathLog',
-        title: 'Mathematical judgments',
-        description: 'Help to proof my mathematical judgments ...'
-    }
-]
 
 const PB = () => {
-
-/*    var marvelHeroes =  heroes.filter(function(hero) {
-        return hero.franchise == “Marvel”;
-    });*/
-    /*let PBTasks = TASKS.map(task => {
-        if(task.type == 'PB')
-            return task
-            /!*task.type.toLowerCase().includes('pb')*!/
-    })*/
     let PBTasks = TASKS.filter(task => task.type=='PB')
 
     return (
