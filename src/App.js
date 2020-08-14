2654359c9bd530e801fd8df8f60a16ea410938b9
Nikita Oksak <stupidmaadworld@gmail.com>
2020-08-14 23:25:58 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Container} from 'react-bootstrap'
 import Header from './components/Header/Header'
 import AllTasks from './components/AllTasks/AllTasks'
 import PB from './components/PB/PB';
-import {BrowserRouter, Route} from 'react-router-dom';
+import Redirect, {BrowserRouter, Route} from 'react-router-dom';
 import MathStat from './components/MathStat/MathStat';
 import MathLog from './components/MathLog/MathLog';
 import Home from './components/Home/Home';
@@ -19,6 +19,7 @@ function App() {
                 <Route path='/PB' component={PB}/>
                 <Route path='/MathStat' component={MathStat}/>
                 <Route path='/MathLog' component={MathLog}/>
+                <Redirect path='/'/>
             </Container>
         </BrowserRouter>
     )
